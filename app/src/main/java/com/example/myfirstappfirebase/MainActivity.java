a7ffaf67038abf9c7239efd2ff40ad692896a797
Alik <alikharutyunyan00@gmail.com>
2024-06-23 22:30:30 +0300

    removed experimental and redundant code

--- app/src/main/java/com/example/myfirstappfirebase/MainActivity.java
+++ app/src/main/java/com/example/myfirstappfirebase/MainActivity.java
@@ -5,39 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.util.List;
 
-import android.util.Log;
-import android.database.Cursor;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.Continuation;
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.functions.FirebaseFunctions;
-import com.google.firebase.functions.HttpsCallableResult;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.google.gson.JsonPrimitive;
 import com.google.mlkit.vision.common.InputImage;
 import com.google.mlkit.vision.label.ImageLabel;
 import com.google.mlkit.vision.label.ImageLabeler;
@@ -48,12 +28,11 @@ import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 public class MainActivity extends AppCompatActivity {
 
     private static final int RESULT_LOAD_IMAGE = 123;
-    private FirebaseFunctions mFunctions;
-    ImageView frame,innerImage;
-    private FirebaseAuth mAuth;
+    ImageView frame, innerImage;
     private Uri image_uri;
     private final ImageLabeler labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
     TextView resultTv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,20 +40,6 @@ public class MainActivity extends AppCompatActivity {
         frame = findViewById(R.id.imageView);
         innerImage = findViewById(R.id.imageView2);
         resultTv = findViewById(R.id.textView);
-        mAuth = FirebaseAuth.getInstance();
-        mAuth.signInWithEmailAndPassword("aliktest@gmail.com", "123456")
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-
-                        } else {
-                            // If sign in fails, display a message to the user.
-
-                    }}
-                });
-        mFunctions = FirebaseFunctions.getInstance();
         frame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
 
-        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null){
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
             image_uri = data.getData();
             innerImage.setImageURI(image_uri);
             Bitmap bitmap = null;
@@ -125,22 +90,8 @@ public class MainActivity extends AppCompatActivity {
         return Bitmap.createScaledBitmap(bitmap, resizedWidth, resizedHeight, false);
     }
 
-    private Task<JsonElement> annotateImage(String requestJson) {
-        return mFunctions
-                .getHttpsCallable("annotateImage")
-                .call(requestJson)
-                .continueWith(new Continuation<HttpsCallableResult, JsonElement>() {
-                    @Override
-                    public JsonElement then(@NonNull Task<HttpsCallableResult> task) {
-                        // This continuation runs on either success or failure, but if the task
-                        // has failed then getResult() will throw an Exception which will be
-                        // propagated down.
-                        return JsonParser.parseString(new Gson().toJson(task.getResult().getData()));
-                    }
-                });
-    }
-    public void doInference(Bitmap input){
-        Bitmap rotated = input ;
+    public void doInference(Bitmap input) {
+        Bitmap rotated = input;
         resultTv.setText("");
         innerImage.setImageBitmap(rotated);
         InputImage image = InputImage.fromBitmap(rotated, 0);
@@ -163,34 +114,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-    }
-    public Bitmap rotateBitmap(Bitmap input){
-        String[] orientationColumn = {MediaStore.Images.Media.ORIENTATION};
-        Cursor cur = getContentResolver().query(image_uri, orientationColumn, null, null, null);
-        int orientation = -1;
-        if (cur != null && cur.moveToFirst()) {
-            orientation = cur.getInt(cur.getColumnIndex(orientationColumn[0]));
-        }
-        Log.d("tryOrientation",orientation+"");
-        Matrix rotationMatrix = new Matrix();
-        rotationMatrix.setRotate(orientation);
-        Bitmap cropped = Bitmap.createBitmap(input,0,0, input.getWidth(), input.getHeight(), rotationMatrix, true);
-        return cropped;
-    }
-    
-    private Bitmap uriToBitmap(Uri selectedFileUri) {
-        try {
-            ParcelFileDescriptor parcelFileDescriptor =
-                    getContentResolver().openFileDescriptor(selectedFileUri, "r");
-            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-
-            parcelFileDescriptor.close();
-            return image;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return  null;
     }
 
 }
